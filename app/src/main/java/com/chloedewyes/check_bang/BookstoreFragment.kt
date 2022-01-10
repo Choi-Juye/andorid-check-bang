603bb551579471c4bff3a7391ef2026ae83f7ebf
chloedewyes <chloedewyes@gmail.com>
2022-01-10 14:16:39 +0900

    feat: 네비게이션과 레이아웃 세팅

---
+++ app/src/main/java/com/chloedewyes/check_bang/BookstoreFragment.kt
@@ -0,0 +1,25 @@
+package com.chloedewyes.check_bang
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.chloedewyes.check_bang.databinding.FragmentBookStoreBinding
+
+class BookstoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentBookStoreBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_book_store, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/chloedewyes/check_bang/MainActivity.kt
+++ app/src/main/java/com/chloedewyes/check_bang/MainActivity.kt
@@ -2,10 +2,21 @@ package com.chloedewyes.check_bang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.chloedewyes.check_bang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
